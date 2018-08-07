e74e78b9046825c5a1e6dbe7f76556d5111ca16c
duc <ducthanhtran@users.noreply.github.com>
2018-08-07 10:15:12 +0200

    basic attention filtering

--- filter.py
+++ filter.py
@@ -35,12 +35,33 @@ if __name__ == '__main__':
     with args.input as f:
         lines = f.read().splitlines()
 
+    max_attention_scores = []
+    headers = []
     for header, matrix in blocks(lines):
         splitted = header.split('|||')
         source = splitted[3].split('<BREAK>')
+        if len(source) == 3:
+            prev2 = source[0].split()
+            prev1 = source[1].split()
+            context_length = len(prev2) + len(prev1)
+        elif len(source) == 2:
+            prev1 = source[0].split()
+            context_length = len(prev1)
+        else:
+            continue
 
-        pass
+        attention_score = np.max(matrix[0:context_length, 0:context_length])
+        max_attention_scores.append(attention_score)
+        headers.append(header)
 
+    # print("Maximum attention score in context sentences: {}".format(np.max(max_attention_scores)))
+    print("Mean attention score in context sentences: {}\n".format(np.mean(max_attention_scores)))
+
+    sorted_indices = (-np.array(max_attention_scores)).argsort()
+    for i in range(10):
+        index = sorted_indices[i]
+        print('attenion score: {}'.format(max_attention_scores[index]))
+        print("Header: {}\n".format(headers[index]))
 
 
 
